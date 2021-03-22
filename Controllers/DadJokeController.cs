b66cc27d9f07b33f1a3c124f6beed8f5eb3e258c
Edward Reilly <reillye@magellanhealth.com>
2021-03-22 19:49:19 -0400

    Initial Commit for Dad joke API

---
+++ Controllers/DadJokeController.cs
@@ -0,0 +1,54 @@
+﻿using DadJokeService.Models;
+using LazyCache;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DadJokeService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DadJokeController : ControllerBase
+    {
+        private readonly IAppCache jokeCache;
+        private readonly IRestClient client;
+        private const string resource = "https://icanhazdadjoke.com/";
+
+        public DadJokeController(IAppCache jokeCache, IRestClient client)
+        {
+            this.jokeCache = jokeCache;
+            this.client = client;
+        }
+
+
+        // GET: api/<DadJokeController>
+        [HttpGet("randomJoke")]
+        public async Task<string> RandomJoke()
+        {
+            var request = new RestRequest(resource);
+            var response = await client.ExecuteGetAsync<RandomJokeResponse>(request);
+
+            jokeCache.Add(response.Data.Id, response.Data.Joke, DateTimeOffset.Now.AddMinutes(20));
+
+            return response.Data.Joke;
+        }
+
+        // GET api/<DadJokeController>/5
+        [HttpGet("{id}")]
+        public async Task<string> Get(string id)
+        {
+            var request = new RestRequest(resource);
+            request.Resource = "j/{id}";
+            request.AddParameter("id", id, ParameterType.UrlSegment);
+            Func<Task<string>> response = async () => (await client.GetAsync<RandomJokeResponse>(request)).Joke;
+
+            var joke = await jokeCache.GetOrAddAsync(id, response);
+
+            return joke;
+        }
+    }
+}
---
+++ Models/RandomJokeResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace DadJokeService.Models
+{
+    public class RandomJokeResponse
+    {
+        public string Id { get; set; }
+        public string Joke { get; set; }
+    }
+}
